daa6d3e45ce5e64ddb27f48ee1ae1e78f3aa5ddf
mongmeo-dev <mongmeo.dev@gmail.com>
2022-07-07 16:50:23 +0900

    initial commit

---
+++ src/test/java/dev/mongmeo/springblog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mongmeo.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
