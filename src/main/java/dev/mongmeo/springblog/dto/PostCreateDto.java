939ec1fdfe0c99fd18c13a4086377703caa7333d
mongmeo-dev <mongmeo.dev@gmail.com>
2022-07-08 03:06:36 +0900

    OpenAPI Schema required 옵션 추가

--- src/main/java/dev/mongmeo/springblog/dto/PostCreateDto.java
+++ src/main/java/dev/mongmeo/springblog/dto/PostCreateDto.java
@@ -14,12 +14,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PostCreateDto {
 
-  @Schema(description = "게시물 제목", example = "게시물 제목입니다.")
+  @Schema(description = "게시물 제목", example = "게시물 제목입니다.", required = true)
   @NotBlank
   @Size(max = 50)
   private String title;
 
-  @Schema(description = "게시물 본문", example = "게시물 내용입니다.")
+  @Schema(description = "게시물 본문", example = "게시물 내용입니다.", required = true)
   @NotBlank
   private String content;
 
