671e53cfc154e13cf2fbfa1a5e11c63d5840431c
mongmeo-dev <mongmeo.dev@gmail.com>
2022-07-07 18:58:27 +0900

    post entity 생성

--- src/main/java/dev/mongmeo/springblog/SpringBlogApplication.java
+++ src/main/java/dev/mongmeo/springblog/SpringBlogApplication.java
@@ -2,8 +2,10 @@ package dev.mongmeo.springblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBlogApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/dev/mongmeo/springblog/entity/PostEntity.java
@@ -0,0 +1,52 @@
+package dev.mongmeo.springblog.entity;
+
+import dev.mongmeo.springblog.dto.PostResponseDto;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+public class PostEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 50)
+  private String title;
+
+  private String content;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  public static PostResponseDto toResponseDto(PostEntity post) {
+    return PostResponseDto.builder()
+        .title(post.getTitle())
+        .content(post.getContent())
+        .createdAt(post.getCreatedAt())
+        .updatedAt(post.getUpdatedAt())
+        .build();
+  }
+}
